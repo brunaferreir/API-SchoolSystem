4393f4812bc705688343865f5e25249b5c1bccf4
Giovana Lopes <gilopes2005@gmail.com>
2025-04-05 17:56:44 -0300

    model turmas

---
+++ app.py
@@ -0,0 +1,9 @@
+import os
+from config import app
+from routesTurma import turmas_blueprint
+
+
+app.register_blueprint(turmas_blueprint)
+
+if __name__ == '__main__':
+  app.run(host=app.config["HOST"], port = app.config['PORT'],debug=app.config['DEBUG'] )
---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from flask import Flask
+
+
+app = Flask(__name__)
+
+app.config['HOST'] = '0.0.0.0'
+app.config['PORT']=5002
+app.config['DEBUG'] = True
